6a0d77871c5b0c41b0487d8de2bf124ff142eec9
Emerson Macedo <emerleite@gmail.com>
2025-05-15 17:58:02 -0300

    NEO Config

---
+++ neo/nvim/lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.api.nvim_set_keymap("n", "<leader>Ao", ":AiderOpen<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>Am", ":AiderAddModifiedFiles<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>Cc", "<cmd>ClaudeCode<CR>", { desc = "Toggle Claude Code" })
